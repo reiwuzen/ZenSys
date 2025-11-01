2959f7d7361eefe29fc1d9081684d4b903a3847b
Rei WuZen <reiwuzen@gmail.com>
2025-11-01 22:05:04 +0530

    feat: update TypeScript configuration and add path aliases
    - Updated tsconfig.json to target ES2022 and added baseUrl and paths for module resolution.
    - Modified tsconfig.node.json to include baseUrl and paths for consistency.
    
    style: add global styles and layout for main components
    
    - Created App.scss for main container styles.
    - Added navbar styles in navbar.scss for layout and design.
    - Implemented Navbar component in navbar.tsx with search and notification icons.
    - Developed home page layout in home.scss and home.tsx, integrating Navbar.
    - Established global styles in global.scss for consistent styling across the application.

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import './home.scss';
+import Navbar from '../../components/navbar/navbar';
+ const HomePage = () => {
+return (
+    <div className="homePage">
+        <Navbar />
+    </div>
+)
+ }
+ export default HomePage
